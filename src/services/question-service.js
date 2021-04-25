cf3888a221dc9828b29f0329a8f4f977c33388ba
rajeshchinnaga <39455723+rajeshchinnaga@users.noreply.github.com>
2021-04-25 11:48:03 -0400

    Changes done to get data from Mongocluster instead of LocalHost

--- src/services/question-service.js
+++ src/services/question-service.js
@@ -1,4 +1,4 @@
-const QUIZZES_URL = 'http://localhost:5000/api/quizzes';
+const QUIZZES_URL = 'https://wbdv-sp21-01-server-node.herokuapp.com/api/quizzes';
 const findQuestionsForQuiz = (qid) => {
     return fetch(`${QUIZZES_URL}/${qid}/questions`)
         .then(response => response.json())
